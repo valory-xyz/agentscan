9f029637dcca98357e5061b545baf0e1726ac9df
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-12-04 12:12:47 -0800

    log event

--- src/components/Onboarding.tsx
+++ src/components/Onboarding.tsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
-import { logEvent } from "@/lib/amplitude";
 
 interface OnboardingProps {
   onStartChat: () => void;
